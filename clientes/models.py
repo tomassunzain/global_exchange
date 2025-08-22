42963513f5aabb2ee48e2a55728dd41f5ae9eb52
tomas.unzain <tomiunzv@gmail.com>
2025-08-22 10:08:15 -0300

    (chore) correccion en models de clientes para migrate

--- clientes/models.py
+++ clientes/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.conf import settings
 
 class Cliente(models.Model):
     SEGMENTOS = [
@@ -10,4 +10,4 @@ class Cliente(models.Model):
 
     nombre = models.CharField(max_length=100)
     tipo = models.CharField(max_length=10, choices=SEGMENTOS, default="MIN")
-    usuario = models.ManyToManyfield(User, related_name="clientes")
+    usuario = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name="clientes")
